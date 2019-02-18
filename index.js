c138d6095bd643e5ba5df8fb7970364340d6ad85
epsalt <evan.saltman@gmail.com>
2019-02-17 21:42:38 -0700

    added timezone support with moment.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-/*global d3, SunCalc*/
+/*global d3, moment, SunCalc*/
 
 const sunchart = () => {
-    const lat = -51;
+    const lat = 51;
     const lon = -114;
+    const tz = "America/Edmonton";
     const year = 2015;
 
     var svg = d3.select("svg"),
@@ -11,29 +12,33 @@ const sunchart = () => {
         padding = 100;
 
     // Contours
-    var d = new Date(year, 0, 1),
-        n = 30,
+    var n = 30,
         mpd = 1440,
         days = 365;
 
-    var data = new Array(mpd/n * days);
+    var m = moment.tz(tz).year(year).month(0).date(1);
+    var data = new Array();
 
-    for (let j = 0; j < days; ++j) {
-        for (let i = 0; i < mpd/n; ++i) {
-            data[j * (mpd/n) + i] = SunCalc.getPosition(d, lat, lon).altitude * 180 / Math.PI;
-            d.setMinutes(d.getMinutes() + n);
+    for (let i = 0; i < days; i++) {
+        for (let j = 0; j < 24; j++) {
+            for (let k = 0; k < 60/n; k++) {
+                m.hours(j).minutes(k * n);
+                var alt = SunCalc.getPosition(m.clone(), lat, lon).altitude * (180/Math.PI);
+                data[i * 24 * (60/n) + j * (60/n) + k] =  alt;
+            }
         }
-    }
+        m.add(1, 'days');
+    };
 
     const projection = d3.geoTransform({
         point: function(x, y) {
             let nx = padding + y * ((width - 2 * padding) / days);
-            let ny = padding + x * ((height - 2 * padding) / (mpd/n));
+            let ny = height - padding -  x * ((height - 2 * padding) / (mpd/n));
             this.stream.point(nx, ny);
         }
     });
 
-    var thresholds = d3.range(-70, 50, 0.25);
+    var thresholds = [-90, -18, -12, -6, 0];
 
     var contours = d3.contours()
         .size([mpd/n, days])
@@ -55,9 +60,8 @@ const sunchart = () => {
 
     var color = d3.scaleLinear()
         .domain(d3.extent(thresholds))
-        .interpolate(d => d3.interpolateMagma);
+        .interpolate(d => d3.interpolateCubehelixDefault);
 
-    // Data Binding
     svg.append('g')
         .selectAll('path')
         .data(contours)
